ed7da1006ac715f36edd767a26d4ab62ee475a6e
Carlos Oseguera <coseguera@live.com>
2025-03-20 17:18:23 -0700

    move modifiers to thumbs

--- keyboards/beekeeb/3w6hs/keymaps/coseguera/keymap.c
+++ keyboards/beekeeb/3w6hs/keymaps/coseguera/keymap.c
@@ -18,10 +18,10 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split_3x5_3(
-        KC_Q,         KC_W,    KC_E,    KC_R,        KC_T,                        KC_Y,    KC_U,    KC_I,           KC_O,           KC_P,  
-        KC_A,         KC_S,    KC_D,    KC_F,        KC_G,                        KC_H,    KC_J,    KC_K,           KC_L,           GUI_T(KC_SCLN),
-        LSFT_T(KC_Z), KC_X,    KC_C,    ALT_T(KC_V), CTL_T(KC_B),                 KC_N,    KC_M,    ALT_T(KC_COMM), CTL_T(KC_DOT),  RSFT_T(KC_SLSH),
-                               MEH_T(KC_EQL), GUI_T(KC_MINS), LT(2, KC_ENT), LT(1, KC_SPC), KC_BSPC, MS_BTN1
+        KC_Q,         KC_W, KC_E, KC_R, KC_T,             KC_Y, KC_U, KC_I,    KC_O,           KC_P,
+        KC_A,         KC_S, KC_D, KC_F, KC_G,             KC_H, KC_J, KC_K,    KC_L,           GUI_T(KC_SCLN),
+        LSFT_T(KC_Z), KC_X, KC_C, KC_V, KC_B,             KC_N, KC_M, KC_COMM, CTL_T(KC_DOT),  RSFT_T(KC_SLSH),
+            CTL_T(KC_EQL), GUI_T(KC_MINS), LT(2, KC_ENT), LT(1, KC_SPC), KC_BSPC, ALT_T(MS_BTN1)
     ),
     [1] = LAYOUT_split_3x5_3(
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  MS_WHLL, MS_WHLD, MS_WHLU, MS_WHLR, KC_BRIU,
         KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10, MS_LEFT, MS_DOWN, MS_UP,   MS_RGHT, KC_BRID,
         KC_MUTE, KC_VOLD, KC_VOLU, KC_F11,  KC_F12, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_BSLS,
-                          XXXXXXX, KC_DEL, _______, MS_BTN1, MS_BTN2, _______
+                          KC_MEH,  KC_DEL, _______, MS_BTN1, MS_BTN2, _______
     )
 };
 
@@ -41,7 +41,6 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case SFT_T(KC_Z):
         case RSFT_T(KC_SLSH):
-        case LT(1, KC_SPC):
             // Immediately select the hold action when another key is pressed.
             return true;
         default:
